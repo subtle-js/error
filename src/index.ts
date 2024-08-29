8dec7299faa30fea1d185685aab3a8eec10ec6f5
Socheat Sok <socheatsok78@gmail.com>
2024-08-30 03:35:33 +0700

    Implement a custom captureStackTrace method

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 export interface CaptureErrorContract {
+    /** Capture error using the container */
     captureError(error: Error): void
 }
 
 interface SubtleErrorInterface {
+    /** Capture error using the container */
     captureError(container: CaptureErrorContract, error: Error): void
+    /** Create .stack property on a target object */
+    captureStackTrace(targetObject: object, constructorOpt?: Function): void;
 }
 
 class SubtleError implements SubtleErrorInterface {
+    captureStackTrace(targetObject: object, constructorOpt?: Function): void {
+        // @ts-ignore
+        if (!Error.captureStackTrace) {
+            Object.defineProperty(targetObject, 'stack', {
+                value: (new Error()).stack,
+                writable: true,
+                configurable: true,
+            })
+            return
+        }
+        // @ts-ignore
+        Error.captureStackTrace(targetObject, constructorOpt)
+    }
     captureError(container: CaptureErrorContract, error: Error): void {
         if (!container.captureError) {
             throw new Error('The "captureError" method not implemented on the container.')
